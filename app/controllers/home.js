6bd7eb956e9d980b3c53740a75ec4ce957a15a69
biancaiz <bianca@luminos.software>
2017-08-17 13:08:56 +0300

    update new record

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,28 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  tasks: Ember.computed(function() {
+    return this.get('store').findAll('task');
+  }),
 
-  todoTasks: Ember.computed('tasks', function() {
-    return this.get('store').peekAll('task').filterBy('status','todo')
+
+  todoTasks: Ember.computed('tasks.length', function() {
+    return this.get('tasks').filterBy('status','todo')
   }),
-  progressTasks: Ember.computed('tasks', function() {
-    return this.get('store').peekAll('task').filterBy('status','progress')
+  progressTasks: Ember.computed('tasks.length', function() {
+    return this.get('tasks').filterBy('status','progress')
   }),
-  doneTasks: Ember.computed('tasks', function() {
-    return this.get('store').peekAll('task').filterBy('status','done')
+  doneTasks: Ember.computed('tasks.length', function() {
+    return this.get('tasks').filterBy('status','done')
   }),
 
   users: Ember.computed(function() {
     return this.get('store').findAll('user');
   }),
-  tasks: Ember.computed(function() {
-    return this.get('store').findAll('task');
-  }),
+
+
   tasktypes: Ember.computed(function() {
     return this.get('store').findAll('tasktype');
   }),
 
   isShowingModal: false,
+
 
   actions: {
     deleteTask(taskid) {
@@ -34,6 +38,7 @@ export default Ember.Controller.extend({
     addTask(taskDescription, userId, tasktypeId){
       const u = this.store.createRecord('task', {
         description: taskDescription,
+        status: "todo",
         user: this.store.peekRecord('user', userId),
         tasktype: this.store.peekRecord('tasktype', tasktypeId)
       }).save();
